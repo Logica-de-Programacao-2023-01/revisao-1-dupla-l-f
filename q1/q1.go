af12644662b2d318f2d6222c8a1581d42c79c557
LÍVIA LEMOS <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2023-04-17 10:33:01 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -7,20 +7,19 @@ func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (floa
 	for i := 0; i < len(purchaseHistory); i++ {
 		somaHistory += purchaseHistory[i]
 	}
-	somaFull := somaHistory + currentPurchase
 	discount := 0.0
 	switch {
 	case currentPurchase <= 0:
 		return 0, fmt.Errorf("Valor inválido")
-	case somaFull == float64(0):
+	case somaHistory == float64(0):
 		discount = currentPurchase * 0.1
-	case somaFull/float64(len(purchaseHistory)) > float64(1000):
+	case somaHistory/float64(len(purchaseHistory)) > float64(1000):
 		discount = currentPurchase * 0.2
-	case somaFull > float64(1000) && somaHistory == float64(0):
+	case somaHistory > float64(1000) || somaHistory == float64(0):
 		discount = currentPurchase * 0.1
-	case somaFull <= float64(1000):
+	case somaHistory <= float64(1000):
 		discount = currentPurchase * 0.05
-	case somaFull <= float64(500):
+	case somaHistory <= float64(500):
 		discount = currentPurchase * 0.02
 	}
 	return discount, nil
