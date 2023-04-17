1f3184a33f320b78bf4642e5fb47505ca6a353e7
Felipe Augusto <126472483+felpino2@users.noreply.github.com>
2023-04-17 17:57:08 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,20 @@
 package q2
 
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	var letras float64
+	var espaços float64
+	if text == "" || text == " " {
+		return 0.0, fmt.Errorf("Texto inválido. ")
+	}
+	for i := 0; i < len(text); i++ {
+		if strings.ContainsAny(text, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") {
+			letras++
+			if letras == 0 {
+				return 0.0, fmt.Errorf("Texto inválido")
+			}
+		}
+	}
+	espaços = float64(len(strings.Split(text, " ")))
+	media := letras / espaços
+	return media, nil
 }
