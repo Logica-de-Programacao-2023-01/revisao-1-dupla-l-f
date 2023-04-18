9d2d7cd580879ee98f5f912362dcd0809b943047
Felipe Augusto <126472483+felpino2@users.noreply.github.com>
2023-04-17 21:30:23 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
 	// Implemente sua solução aqui
-	return 0, 0, 0, nil
-	var Max, Min int 
+	var Max, Min int
 	var soma, embaixo, media float64
 	if len(numbers) == 0 {
 		return 0, 0, 0, fmt.Errorf("Lista vazia")
@@ -13,12 +12,12 @@ func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
 	for i := 0; i < len(numbers); i++ {
 		soma += float64(numbers[i])
 		embaixo++
-		if numbers[i] > Max{
+		if numbers[i] > Max {
 			Max = numbers[i]
 		} else if numbers[i] < Min {
 			Min = numbers[i]
 		}
 	}
-media = soma/embaixo
+	media = soma / embaixo
 	return Max, Min, media, nil
 }
