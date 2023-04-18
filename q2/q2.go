d5f17fbb6981f0b2a9c3d155bbe2f15ee6f2f5fd
Felipe Augusto <126472483+felpino2@users.noreply.github.com>
2023-04-18 15:29:23 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -6,21 +6,42 @@ import (
 )
 
 func AverageLettersPerWord(text string) (float64, error) {
-	var letras float64
-	var espaços float64
-	if text == "" || text == " " {
-		return 0.0, fmt.Errorf("Texto inválido. ")
-	}
-	for i := 0; i < len(text); i++ {
-		if strings.ContainsAny(text, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") {
-			letras++
-			if letras == 0 {
-				return 0.0, fmt.Errorf("Texto inválido")
-			}
-		}
-	}
-	espaços = float64(len(strings.Split(text, " ")))
-	media := letras / espaços
+
+	numerodeletras := 0
+
+
+	text1 := strings.ReplaceAll(text, "!", "")
+	text1 = strings.ReplaceAll(text1, ",", "")
+	text1 = strings.ReplaceAll(text1, ".", "")
+	text1 = strings.ReplaceAll(text1, "(", "")
+	text1 = strings.ReplaceAll(text1, ")", "")
+	text1 = strings.ReplaceAll(text1, "[", "")
+	text1 = strings.ReplaceAll(text1, "]", "")
+	text1 = strings.ReplaceAll(text1, "{", "")
+	text1 = strings.ReplaceAll(text1, "}", "")
+	text1 = strings.ReplaceAll(text1, "?", "")
+	text1 = strings.ReplaceAll(text1, "0", "")
+	text1 = strings.ReplaceAll(text1, "1", "")
+	text1 = strings.ReplaceAll(text1, "2", "")
+	text1 = strings.ReplaceAll(text1, "3", "")
+	text1 = strings.ReplaceAll(text1, "4", "")
+	text1 = strings.ReplaceAll(text1, "5", "")
+	text1 = strings.ReplaceAll(text1, "6", "")
+	text1 = strings.ReplaceAll(text1, "7", "")
+	text1 = strings.ReplaceAll(text1, "8", "")
+	text1 = strings.ReplaceAll(text1, "9", "")
+
+for _, letra := range text1 {
+	if unicode.IsLetter(letra) {
+	numerodeletras++}
+}
+if numerodeletras == 0 {
+	return 0, fmt.Errorf("Erro ")
+}
+numerodepalavras := len(strings.Fields(text1))
+
+media := float64(numerodeletras)/float64(numerodepalavras)
+
 	return media, nil
 }
 
