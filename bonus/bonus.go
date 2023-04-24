9bd397ffe2fc0d6718be31c2d147235d420ee7cb
LÍVIA LEMOS <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2023-04-23 22:52:21 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -16,31 +16,29 @@ func CalculateDamage(characterLevel, enemyLevel int) (int, error) {
 		} else if (characterLevel - enemyLevel) < 20 {
 			damage = characterLevel * 2
 		}
-	} else {
-		if characterLevel < enemyLevel {
-			damage = characterLevel * 5
-			if characterLevel == enemyLevel {
-				damage = characterLevel * 7
-			} else if enemyLevel > 100 {
-				damage = characterLevel * 2
-			}
-		}
-	} else {
-		switch {
-		case characterLevel > enemyLevel:
-			damage = characterLevel * 5
-		case characterLevel == enemyLevel:
+	} else if characterLevel < enemyLevel {
+		damage = characterLevel * 5
+		if characterLevel == enemyLevel {
 			damage = characterLevel * 7
-		case enemyLevel > 100:
-		case characterLevel > 100:
-			damage = characterLevel * 20
-		case enemyLevel > 100:
-			damage = characterLevel * 2
-		case (characterLevel - enemyLevel) > 20:
-			damage = characterLevel * 5
-		case (characterLevel - enemyLevel) < 20:
+		} else if enemyLevel > 100 {
 			damage = characterLevel * 2
 		}
-	}
-	return damage, nil
+	} else {
+switch {
+case characterLevel > enemyLevel:
+damage = characterLevel * 5
+case characterLevel == enemyLevel:
+damage = characterLevel * 7
+case enemyLevel > 100:
+case characterLevel > 100:
+damage = characterLevel * 20
+case enemyLevel > 100:
+damage = characterLevel * 2
+case (characterLevel - enemyLevel) > 20:
+damage = characterLevel * 5
+case (characterLevel - enemyLevel) < 20:
+damage = characterLevel * 2
+}
+}
+return damage, nil
 }
