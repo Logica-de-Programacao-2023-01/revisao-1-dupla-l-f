7e7ea4e247a07aad23a9a22707f82e4ac4e19285
LÍVIA LEMOS <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2023-04-23 21:53:43 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,18 +1,22 @@
 func CalculateDamage(characterLevel, enemyLevel int) (int, error) {
 	var damage int
-	switch {
-	case characterLevel > enemyLevel:
-		damage = characterLevel * 10
-	case characterLevel < enemyLevel:
-		damage = characterLevel * 5
-	case characterLevel == enemyLevel:
-		damage = characterLevel * 7
-	case enemyLevel > 100:
-		damage = characterLevel * 20
-	case (characterLevel - enemyLevel) > 20:
-		damage = characterLevel * 5
-	case (characterLevel - enemyLevel) < 20:
-		damage = characterLevel * 2
+	if characterLevel <=0 && enemyLevel <=0 {
+		fmt.Errorf("Nível inválido.")
+	} else {
+		switch {
+		case characterLevel > enemyLevel:
+			damage = characterLevel * 10
+		case characterLevel < enemyLevel:
+			damage = characterLevel * 5
+		case characterLevel == enemyLevel:
+			damage = characterLevel * 7
+		case enemyLevel > 100:
+			damage = characterLevel * 20
+		case (characterLevel - enemyLevel) > 20:
+			damage = characterLevel * 5
+		case (characterLevel - enemyLevel) < 20:
+			damage = characterLevel * 2
+		}
 	}
 	return damage, nil
 }
